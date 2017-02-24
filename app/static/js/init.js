292851d6f84d2fc001afdda7c40a2c08331e4dcf
Oskar Wirga <oskar@wirga.com>
2017-02-24 18:37:24 -0500

    Updated modals and challenge page

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -5,5 +5,6 @@
     $('.tooltipped').tooltip({delay: 50});
     $('.modal').modal();
 	$('select').material_select();
+	$('.collapsible').collapsible();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
