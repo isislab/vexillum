854442a2f5220398cf16dd437e1b1542a7e89d86
Oskar Wirga <oskar@wirga.com>
2017-02-23 22:55:17 -0500

    Added/Updated Files with modals/FAB

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -2,6 +2,7 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-
+    $('.modal').modal();
+	$('select').material_select();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
