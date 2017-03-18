05cb5d77a52403aa53bfd1d994e4bd0f5936a525
Oskar Wirga <oskar@wirga.com>
2017-03-17 19:05:36 -0700

    New Chal Data and Event Page

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -1,10 +1,23 @@
-(function($){
-  $(function(){
+(function($) {
+    var clock
+    $(function() {
 
-    $('.button-collapse').sideNav();
-    $('.tooltipped').tooltip({delay: 50});
-    $('.modal').modal();
-	$('select').material_select();
-	$('.collapsible').collapsible();
-  }); // end of document ready
+        $('.button-collapse').sideNav();
+        $('.tooltipped').tooltip({
+            delay: 50
+        });
+        $('.modal').modal();
+        $('select').material_select();
+        $('.collapsible').collapsible();
+        var clock;
+
+        clock = $('.clock').FlipClock({
+            clockFace: 'DailyCounter',
+            autoStart: false,
+        });
+
+        clock.setTime(220880);
+        clock.setCountdown(true);
+        clock.start();
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
